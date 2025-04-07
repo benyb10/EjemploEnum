e206a643285996a0fa851dec861d9d174b80c22c
bryan <bryan@DESKTOP-CHUEF63>
2025-04-07 16:48:49 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace EjemploEnum
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbEstado = new ComboBox();
+            btnMostrar = new Button();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // cmbEstado
+            // 
+            cmbEstado.FormattingEnabled = true;
+            cmbEstado.Location = new Point(380, 93);
+            cmbEstado.Name = "cmbEstado";
+            cmbEstado.Size = new Size(106, 23);
+            cmbEstado.TabIndex = 0;
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.Location = new Point(285, 93);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(75, 23);
+            btnMostrar.TabIndex = 1;
+            btnMostrar.Text = "Mostrar Resultado";
+            btnMostrar.UseVisualStyleBackColor = true;
+            btnMostrar.Click += btnMostrar_Click;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(305, 139);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(0, 15);
+            lblResultado.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResultado);
+            Controls.Add(btnMostrar);
+            Controls.Add(cmbEstado);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbEstado;
+        private Button btnMostrar;
+        private Label lblResultado;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+namespace EjemploEnum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Cargar enum en ComboBox
+            cmbEstado.DataSource = Enum.GetValues(typeof(EstadoPedido));
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            // Obtener valor seleccionado del ComboBox
+            EstadoPedido estadoSeleccionado = (EstadoPedido)cmbEstado.SelectedItem;
+
+            switch (estadoSeleccionado)
+            {
+                case EstadoPedido.Pendiente:
+                    lblResultado.Text = "Tu pedido está pendiente.";
+                    break;
+                case EstadoPedido.Procesando:
+                    lblResultado.Text = "Tu pedido está siendo procesado.";
+                    break;
+                case EstadoPedido.Enviado:
+                    lblResultado.Text = "Tu pedido ha sido enviado.";
+                    break;
+                case EstadoPedido.Entregado:
+                    lblResultado.Text = "Tu pedido fue entregado.";
+                    break;
+                case EstadoPedido.Cancelado:
+                    lblResultado.Text = "Tu pedido fue cancelado.";
+                    break;
+                default:
+                    lblResultado.Text = "Estado desconocido.";
+                    break;
+
+            }
+            }
+    }
+}
